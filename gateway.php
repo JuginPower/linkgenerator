e3e924f92b23b88ab370ad2a39266fc8a80e0f9a
JuginPower <juginpower90@gmail.com>
2022-04-17 23:19:39 +0200

    Feierabend-Commit

--- gateway.php
+++ gateway.php
@@ -1,7 +1,7 @@
 <?php 
+/* 
 $data = json_decode(file_get_contents('php://input'), true);
-
-// $links = array("strings"=>$data["string-quantity"], "keywords"=>$data["keyword-quantity"]);
-// echo json_encode($links);
+$links = array("strings"=>$data["string-quantity"], "keywords"=>$data["keyword-quantity"]);
+echo json_encode($links); */
 
 ?>
