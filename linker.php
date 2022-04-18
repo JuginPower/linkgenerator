706db5ea4f07312a4c6bf19f70ed154cf50c88f0
JuginPower <juginpower90@gmail.com>
2022-04-18 16:22:46 +0200

    tables result

--- linker.php
+++ linker.php
@@ -8,8 +8,8 @@ function cleaner($roughdata) {
     foreach (array_keys($roughdata) as $datakey) {
 
         $newkey = "\{" + strval($counter) + "\}";
-        $item = $roughdata[$datakey];
-        $op_array = array($newkey => $item);
+        $items = clone $roughdata[$datakey];
+        $op_array = array($newkey => $items);
         array_push($new_assoc, $op_array);
         $counter ++;
     }
@@ -19,9 +19,30 @@ function cleaner($roughdata) {
 
 function link_generator($string, $data) {
     
-    $progress = array();
+    $progress_array = array();
     $clean_data = cleaner($data);
 
+    foreach (array_keys($clean_data) as $cdkey) {
+
+        $values_list = clone $clean_data[$cdkey];
+
+        if (!empty($progress_array)) {
+
+            foreach ($values_list as $val) {
+                $progress_string = array_shift($progress_array);
+                $further_progress_string = preg_replace($cdkey, strval($val), $progress_string);
+                array_push($progress_array, $further_progress_string);
+
+            }
+        } else {
+            foreach ($values_list as $val) {
+
+                $formated_string = preg_replace($cdkey, strval($val), $string);
+                array_push($progress_array, $formated_string);
+            }
+        }
+    }
+    return $progress_array;
 }
 
 ?>
